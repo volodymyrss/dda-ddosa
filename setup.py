b95b423d04fb3e2055817647d99107a384d13668
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-09-19 14:51:33 +0000

    ddosa package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ddosa',
+    version='1.0',
+    packages=['ddosa',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    #long_description=open('README.md').read(),
+)
