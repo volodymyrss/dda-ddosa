e3cd8c606e8c6e09b9386f470a1d0e7a1d1e2536
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-09-19 16:17:56 +0000

    pack

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ddosa',
     version='1.0',
-    packages=['ddosa',],
+    py_modules=['ddosa',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     #long_description=open('README.md').read(),
 )
