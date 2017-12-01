869362e1d6a6bf92bcf65c9e7de96ff15a48d660
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-12-01 14:40:48 +0100

    float set_attr

--- ddosa.py
+++ ddosa.py
@@ -1997,6 +1997,7 @@ class lc_pick(DataAnalysis):
     source_names=["Crab"]
 
     cached=True
+    instrument="isgri"
 
     def get_version(self):
         try:
@@ -2016,7 +2017,7 @@ class lc_pick(DataAnalysis):
             ht = heatool("lc_pick")
             ht['group'] = "ogg.fits[1]"
             ht['source']=source_name
-            ht['instrument']="isgri"
+            ht['instrument']=self.instrument
             ht['lc']=fn
             ht.run()
             setattr(self,'lightcurve',da.DataFile(fn))
@@ -2518,7 +2519,7 @@ def import_attr(obj,attr,target="og.fits"):
     
 def set_attr(attrs,fn="og.fits"):
 
-    pt2dt={int:"DAL_INT",str:"DAL_CHAR"}
+    pt2dt={int:"DAL_INT",str:"DAL_CHAR", float:'DAL_FLOAT'}
     pt2k={int:"i",str:"s"}
 
     for k,v in attrs.items():
