a92bb3692cddd406cfd7f48461e826222317de3c
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-01-30 15:42:24 +0100

    fitting evo

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='ddosa',
-    version='1.0',
-    py_modules=['ddosa',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    #long_description=open('README.md').read(),
-)
